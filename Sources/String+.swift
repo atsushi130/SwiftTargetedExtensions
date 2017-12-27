73a1d3509c5bb62f345a3308d43159e6e712206d
atsushi130 <atsushi_main@outlook.jp>
2017-12-27 17:30:31 +0900

    Add: reguler expression method to String

--- Sources/String+.swift
+++ Sources/String+.swift
@@ -14,4 +14,13 @@ public extension Extension where Base == String {
     public func toDate(by format: String = "yyyy/MM/dd HH:mm:ss", locale: Locale = Locale.current) -> Date? {
         return DateFormatter.ex.from(locale: locale, format: format).date(from: self.base)
     }
+    
+    public func isMatch(pattern: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
+            return regex.firstMatch(in: self.base, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.base.count)) != nil
+        } catch {
+            return false
+        }
+    }
 }
