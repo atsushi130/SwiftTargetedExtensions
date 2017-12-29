b6dc01dd8ca81a319431cd588f5a1fbbd27a8768
atsushi130 <atsushi_main@outlook.jp>
2017-12-29 22:39:58 +0900

    Add: Double extension

--- Sources/Double+.swift
+++ Sources/Double+.swift
@@ -2,8 +2,29 @@
 //  Double+.swift
 //  SwiftExtensions
 //
-//  Created by 三宅　篤史 on 2017/12/29.
+//  Created by Atsushi Miyake on 2017.
 //  Copyright © 2017年 Atsushi Miyake. All rights reserved.
 //
 
 import Foundation
+
+extension Double: ExtensionCompatible {}
+
+extension Extension where Base == Double {
+    
+    var half: Double {
+        return self.base / 2.0
+    }
+    
+    var floor: Double {
+        return Foundation.floor(self.base)
+    }
+    
+    var ceil: Double {
+        return Foundation.ceil(self.base)
+    }
+    
+    var round: Double {
+        return Foundation.round(self.base)
+    }
+}
