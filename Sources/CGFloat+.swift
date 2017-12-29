df066f0d6d9c5b467b69b1b5056da1f8bb179074
atsushi130 <atsushi_main@outlook.jp>
2017-12-29 22:39:43 +0900

    Add: CGFloat extension

---
+++ Sources/CGFloat+.swift
@@ -0,0 +1,30 @@
+//
+//  CGFloat+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2017.
+//  Copyright © 2017年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+extension CGFloat: ExtensionCompatible {}
+
+extension Extension where Base == CGFloat {
+    
+    var half: CGFloat {
+        return self.base / 2.0
+    }
+    
+    var floor: CGFloat {
+        return Foundation.floor(self.base)
+    }
+    
+    var ceil: CGFloat {
+        return Foundation.ceil(self.base)
+    }
+    
+    var round: CGFloat {
+        return Foundation.round(self.base)
+    }
+}
---
+++ Sources/Double+.swift
@@ -0,0 +1,9 @@
+//
+//  Double+.swift
+//  SwiftExtensions
+//
+//  Created by 三宅　篤史 on 2017/12/29.
+//  Copyright © 2017年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
