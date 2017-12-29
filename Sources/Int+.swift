b7ab0ae8ad894e994bbaf3cc15e6a86ac52d305d
atsushi130 <atsushi_main@outlook.jp>
2017-12-29 22:40:12 +0900

    Add: Int extension

--- Sources/Int+.swift
+++ Sources/Int+.swift
@@ -2,8 +2,17 @@
 //  Int+.swift
 //  SwiftExtensions
 //
-//  Created by 三宅　篤史 on 2017/12/29.
+//  Created by Atsushi Miyake on 2017.
 //  Copyright © 2017年 Atsushi Miyake. All rights reserved.
 //
 
 import Foundation
+
+extension Int: ExtensionCompatible {}
+
+extension Extension where Base == Int {
+    
+    var half: Double {
+        return Double(self.base) / 2.0
+    }
+}
