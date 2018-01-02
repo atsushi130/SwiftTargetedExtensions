b1ea0bbe15e7ddeea50679677751019abdfc6c43
atsushi130 <atsushi_main@outlook.jp>
2018-01-03 05:06:41 +0900

    reimplement NibInstantiatable and add NibDesignable

---
+++ Sources/NibDesignable.swift
@@ -0,0 +1,26 @@
+//
+//  NibDesignable.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2018/01/03.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+protocol NibDesignable: class {}
+
+extension NibDesignable where Self: UIView {
+    
+    private func instantiate() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib    = UINib(nibName: self.ex.className, bundle: bundle)
+        return nib.instantiate(withOwner: self).first as? UIView ?? UIView()
+    }
+    
+    func configureNib() {
+        let view = self.instantiate()
+        self.insertSubview(view, at: 0)
+        view.ex.fillSuperview()
+    }
+}
