66eaba78735be1ea2086fa589492a59343d323c1
atsushi130 <atsushi_main@outlook.jp>
2018-01-03 00:33:56 +0900

    add EmbeddedNibInstantiatable

--- Sources/EmbeddedNibInstantiatable.swift
+++ Sources/EmbeddedNibInstantiatable.swift
@@ -2,8 +2,21 @@
 //  EmbeddedNibInstantiatable.swift
 //  SwiftExtensions
 //
-//  Created by Atsushi Miyake on 2018/01/02.
+//  Created by Atsushi Miyake on 2018.
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+public protocol EmbeddedNibInstantiatable {
+    associatedtype Embedded: NibInstantiatable
+}
+
+public extension EmbeddedNibInstantiatable where Self: UIView, Embedded: UIView {
+    public var embedded: Embedded { return subviews[0] as! Embedded }
+    
+    public func configureEmbededView() {
+        let view = Embedded.instantiate()
+        insertSubview(view, at: 0)
+        view.ex.fillSuperview()
+    }
+}
+
