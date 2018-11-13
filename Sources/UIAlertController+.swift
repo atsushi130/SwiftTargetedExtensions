58df8c685e420a084628c2e73c9e6a69ce7e37e9
atsushi130 <atsushi_main@outlook.jp>
2018-11-13 23:55:39 +0900

    add: UIAlertController+.swift

---
+++ Sources/UIAlertController+.swift
@@ -0,0 +1,9 @@
+//
+//  UIAlertController+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2018/11/13.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
