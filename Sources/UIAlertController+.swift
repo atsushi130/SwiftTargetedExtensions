28bd1a735020470bbd05b6f02c4eb51fca2f6724
atsushi130 <atsushi_main@outlook.jp>
2018-11-13 23:56:30 +0900

    implement: okAlertController

--- Sources/UIAlertController+.swift
+++ Sources/UIAlertController+.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public extension UIAlertController {
+    public static func okAlertController(title: String = "", message: String = "") -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        return alertController
+    }
+}
