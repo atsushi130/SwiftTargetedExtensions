e5b55a9d746c83e3feee7e7f46ce9ee97fe7390d
atsushi130 <atsushi_main@outlook.jp>
2019-02-11 16:57:02 +0900

    add: JSONDecoder extension for snake case strategy

---
+++ Sources/JSONDecoder+.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDecoder+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2019/02/11.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public extension JSONDecoder {
+    public static var snakeCaseDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+}
